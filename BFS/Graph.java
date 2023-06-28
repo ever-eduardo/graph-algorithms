a5a40a47061064084f0a5209b91c7c2ba55ab1e3
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2023-06-27 21:36:54 -0500

    Create Graph.java

---
+++ BFS/Graph.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+    private final int V;
+    private final List<List<Integer>> adjacencyList; // Adjacency List Representation.
+
+    public Graph(int vertices) {
+        V = vertices;
+        adjacencyList = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    public int getVerticesCount() {
+        return V;
+    }
+
+    public List<List<Integer>> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public void addEdge(int v, int w) {
+        adjacencyList.get(v).add(w);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Graph Adjacency List:\n");
+        result.append("[\n");
+        for (List<Integer> vector: adjacencyList) {
+            result.append("\t");
+            result.append(vector.toString());
+            result.append("\n");
+        }
+        result.append("]");
+        return result.toString();
+    }
+}
